371d1bf26771b8f0cc838e88942b2ac05af2e8fc
Ericles Willian <ewnsilva@gmail.com>
2026-08-15 18:13:35 -0300

    refactor: adding categories constant

---
+++ src/data/categories.ts
@@ -0,0 +1,40 @@
+import { Category } from "./types";
+
+export const categories: Category[] = [
+  {
+    id: "star",
+    code: "FAVORITE",
+    name: "Destaques",
+    description: "Os preferidos dos viajantes",
+  },
+  {
+    id: "urban",
+    code: "URBAN",
+    name: "Urbano",
+    description: "Explore a vida na cidade",
+  },
+  {
+    id: "beach",
+    code: "BEACH",
+    name: "Praia",
+    description: "Sombra e água fresca",
+  },
+  {
+    id: "nature",
+    code: "NATURE",
+    name: "Natureza",
+    description: "Os melhores destinos para descansar",
+  },
+  {
+    id: "culture",
+    code: "CULTURE",
+    name: "Cultura",
+    description: "História, arte e tradição",
+  },
+  {
+    id: "adventure",
+    code: "ADVENTURE",
+    name: "Aventura",
+    description: "Para quem gosta de emoção",
+  },
+];
